664add9fdb6b0918f8f2c228929b79c9ba1be97d
greatdistance <18985650250@163.com>
2019-08-04 15:19:15 +0800

    mybatis 逆向生成器

---
+++ src/main/java/cn/com/bonc/dao/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.com.bonc.dao.mapper;
+
+import cn.com.bonc.dao.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
